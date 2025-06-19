64f878038676da15bebe5787812c5739db3c2b59
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-06-20 00:06:51 +0500

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,15 @@
+# In Python, decorators are a powerful and flexible way to modify or extend 
+# the behavior of functions or methods, without changing their actual code
+
+def decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@decorator
+def fun():
+    print("Hello Usman")
+
+fun()
