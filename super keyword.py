3a9629d4bf3526262ef8e8d649e8fc1ecc6447ca
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-07-15 23:56:52 +0500

    Add files via upload

---
+++ super keyword.py
@@ -0,0 +1,16 @@
+class A:
+    def display(self):
+        print("Display from class A")
+class B(A):
+    def display(self):
+        print("Display from class B")
+        super().display()  # Calls the display method from class A
+class C(B):
+    def display(self):
+        print("Display from class C")
+        super().display()  # Calls the display method from class B
+c= C()
+c.display()
+#b = B()
+#b.display()  # This will print "Display from class B" followed
+
