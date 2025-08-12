3c7f3449153a0e41e760b4466d110bb9a642a393
Usman Ashfaq <usmanashfaq104@gmail.com>
2025-08-12 21:49:09 +0500

    correct the indendation

--- loops.py
+++ loops.py
@@ -1,29 +1,31 @@
-number=4
-#use of while loop
-while(number>0):
-    number=number-1
+number = 4
+
+# use of while loop
+while number > 0:
+    number = number - 1
     print("Usman")
 print("Next")
-#use of for loop
-#using the list
-li=["Usman","Ashfaq"]
-for i in li:                #in keyword check whether i is in range
+
+# use of for loop
+li = ["Usman", "Ashfaq"]
+for i in li:
     print(i)
-for i in range(0,4):
+for i in range(0, 4):
     print("Hello")
- 
- #use of cases or switch
+
+# use of cases or switch
 match number:
- case 1:
-    print("Number conataains 4!")
- case 2:
-    print("It is 2")
- case 3|4:
-    print("It contains 4")    
- case _:
-    print("Other number, 4 is not not present!")
-#loop within loop
+    case 1:
+        print("Number contains 4!")
+    case 2:
+        print("It is 2")
+    case 3:
+        print("It contains 4") 
+    case 4:
+        print("Other number, 4 is not present!")
+
+# loop within loop
 for i in range(1, 5):
     for j in range(i):
-        print(i,end=' ')
+        print(i, end=' ')
     print()
