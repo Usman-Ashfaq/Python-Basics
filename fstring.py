e518cfb02c7b851449cced9216fb66f6b18ed65c
Usman Ashfaq <usmanashfaq104@gmail.com>
2025-08-13 22:48:54 +0500

    Added

--- fstring.py
+++ fstring.py
@@ -2,7 +2,10 @@ st=("My name is {0} and I read in {1}")
 name="Ch Usman"
 uni=("Fast Uni ")
 print(st.format(name,uni))
-#using f string now
+#using f string now 
 print(f"My name is {name} and I read in {uni}")
 #to retain what is string
 print(f"My name is {{name}} and I read in {{uni}}")
+#using f string with variables
+age=20
+print(f"My name is {name}, I read in {uni} and my age is {age}")
